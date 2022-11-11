ae5140dbe49a67a26936a2b938f7e79fd92600a4
Tatyana <tatyana.volkova1984@mail.ru>
2022-11-11 20:30:59 +0500

    max folder created

---
+++ max/Program.cs
@@ -0,0 +1,16 @@
+﻿ Console.WriteLine("максимальное значение для двух целых чисел");
+            Console.Write("Введите первое число ");
+            double a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите второе число ");
+            double b = Convert.ToInt32(Console.ReadLine());
+           double max;
+           if (a > b)
+           {
+               max = a;
+               
+           }
+           else
+           {
+               max = b;
+           }
+           Console.WriteLine("макс число :{0}", max);
