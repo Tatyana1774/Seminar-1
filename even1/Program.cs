9a972319d8050e8c1ef0022676c12b3822c59c68
Tatyana <tatyana.volkova1984@mail.ru>
2022-11-12 02:17:23 +0500

    all even numbers from 1 to N

---
+++ even1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Напишите цифру ");
+        int n = 1;
+        int m = int.Parse(Console.ReadLine());
+        for (int i = n; i <= m; i++)
+        {
+            if (i % 2 == 0) 
+            {
+                Console.Write(i + " ");
+            }
+        }
+        Console.ReadKey();
