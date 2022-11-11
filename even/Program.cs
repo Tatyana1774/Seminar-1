fc6ada1b388a3e2bb903fc97caad2e2e3e38c7b9
Tatyana <tatyana.volkova1984@mail.ru>
2022-11-11 21:33:09 +0500

    added folder even

---
+++ even/Program.cs
@@ -0,0 +1,17 @@
+﻿ int i;
+ Console.WriteLine("Введите число ");
+ i = Convert.ToInt32(Console.ReadLine());
+ 
+if (i % 2 == 0)
+ {
+
+ Console.Write("Введенное число является четным");
+ Console.Read();
+ }
+
+ else
+ {
+
+ Console.WriteLine("Введенное число нечетное");
+ Console.Read();
+ }
